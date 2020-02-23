838b0f4b77b2cd9ed246edd2e0247bd77efbc5fb
Jerry Wang <focaaby@gmail.com>
2020-02-23 15:16:21 +0000

    Implement q_insert_head(), q_insert_tail, q_size()

--- queue.c
+++ queue.c
@@ -12,8 +12,10 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q == NULL)
+        return NULL;
+    q->head = q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -34,13 +36,21 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL)
+        return false;
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (newh == NULL)
+        return false;
+    newh->value = malloc(sizeof(char) * (strlen(s) + 1));
+    memset(newh->value, 0, sizeof(char) * (strlen(s) + 1));
+    strncpy(newh->value, s, strlen(s));
+    newh->next = NULL;
     newh->next = q->head;
     q->head = newh;
+    if (q->size == 0)
+        q->tail = newh;
+    q->size++;
     return true;
 }
 
@@ -53,10 +63,24 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (q == NULL)
+        return false;
+    list_ele_t *newt;
+    newt = malloc(sizeof(list_ele_t));
+    if (newt == NULL)
+        return false;
+    newt->value = malloc(sizeof(char) * (strlen(s) + 1));
+    memset(newt->value, 0, sizeof(char) * (strlen(s) + 1));
+    strncpy(newt->value, s, strlen(s));
+    newt->next = NULL;
+    if (q->tail != NULL) {
+        q->tail->next = newt;
+    } else {
+        q->head = newt;
+    }
+    q->tail = newt;
+    q->size++;
+    return true;
 }
 
 /*
@@ -81,9 +105,9 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
+    /* TODO: It should operate in O(1) time */
+    if (q != NULL)
+        return q->size;
     return 0;
 }
 

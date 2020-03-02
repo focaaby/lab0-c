34a93ce9cd4f97c43e64be24da6f1853e782cc2b
Jerry Wang <focaaby@gmail.com>
2020-03-02 15:47:39 +0000

    Implement q_free()

--- queue.c
+++ queue.c
@@ -22,8 +22,17 @@ queue_t *q_new()
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (!q)
+        return;
+    list_ele_t *tmp;
+    while (q->head) {
+        tmp = q->head->next;
+        free(q->head->value);
+        free(q->head);
+        q->head = tmp;
+    }
+    free(tmp);
     free(q);
 }
 
